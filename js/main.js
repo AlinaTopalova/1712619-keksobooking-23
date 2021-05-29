c1d658fd1e9a4dc16fada4ad28f74d8bd5daa8b4
Герман Топалов <alinatopalova7@gmail.com>
2021-05-29 15:17:45 +0200

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const getRandomNumber = function (min, max) {
+  if (max > min && min >= 0 && max > 0) {
+    return Math.floor(min + Math.random() * (max + 1 - min))
+  } console.log('Ошибочные значения диапазона: первое значение должно быть меньше второго, а также диапазон может быть только положительный, включая ноль');
+};
+
+
+const getRandomNoninteger = function (min, max, decimalNumber) {
+  if (max > min && min >= 0 && max > 0) {
+    return (min + Math.random() * (max - min)).toFixed(decimalNumber)
+  }
+  console.log('Ошибочные значения диапазона: первое значение должно быть меньше второго, а также диапазон может быть только положительный, включая ноль');
+};
+
+
+getRandomNumber(10, 62);
+getRandomNoninteger(5, 10, 9);
