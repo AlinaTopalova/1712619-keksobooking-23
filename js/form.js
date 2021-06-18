f6d8c4cb1c8e01bbaf8eaec57c3111e0f62aec84
Герман Топалов <alinatopalova7@gmail.com>
2021-06-18 22:20:41 +0200

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -1,5 +1,25 @@
+const MIN_TITLE_LENGTH = 2;
+const MAX_TITLE_LENGTH = 100;
+
 const form = document.querySelector('.ad-form');
 const formFieldsets = form.querySelectorAll('fieldset');
+const formAdTitle = form.querySelector('#title');
+const roomsSelect = form.querySelector('#room_number');
+const guestsSelect = form.querySelector('#capacity');
+
+const Rooms = {
+  ONE: '1',
+  TWO: '2',
+  THREE: '3',
+  HUNDREED: '100',
+};
+
+const Guests = {
+  ONE: '1',
+  TWO: '2',
+  THREE: '3',
+  ZERO: '0',
+};
 
 const disableAdForm = () => {
   form.classList.add('ad-form--disabled');
@@ -15,5 +35,39 @@ const enableAdForm = () => {
     fieldset.disabled = false;
   });
 };
+
+const validateCapacity = () => {
+  const roomsNumber = roomsSelect.value;
+  const guestsNumber = guestsSelect.value;
+  let error = '';
+
+  if (roomsNumber === Rooms.ONE && guestsNumber !== Guests.ONE) {
+    error += 'Только для одного гостя';
+  } else if (roomsNumber === Rooms.TWO && guestsNumber !== Guests.ONE && guestsNumber !== Guests.TWO) {
+    error += 'Только для одного или двух гостей';
+  } else if (roomsNumber === Rooms.THREE && guestsNumber === Guests.ZERO) {
+    error += 'Только для одного, двух или трёх гостей';
+  } else if (roomsNumber === Rooms.HUNDREED && guestsNumber !== Guests.ZERO) {
+    error += 'Не для гостей';
+  }
+  guestsSelect.setCustomValidity(error);
+  guestsSelect.reportValidity();
+};
+
+const validateTitle = () => {
+  const valueLength = formAdTitle.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    formAdTitle.setCustomValidity(`Еще ${MIN_TITLE_LENGTH - valueLength} симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    formAdTitle.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`);
+  } else {
+    formAdTitle.setCustomValidity('');
+  }
+  formAdTitle.reportValidity();
+};
+
+formAdTitle.addEventListener('input', validateTitle);
+form.addEventListener('change', validateCapacity);
 
 export {disableAdForm, enableAdForm};
